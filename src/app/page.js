5f279b418965dcb5e90d88d7857c4a8ee7a5f269
Guuzta <Guuhsilva13@outlook.com>
2026-01-25 21:42:07 -0300

    adicionando Loader na pagina home

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,13 @@ import { useVerifyToken } from "../hooks/verifyToken";
 
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import Loader from "@/components/Loader";
 
 export default function Home() {
   const authenticated = useVerifyToken();
 
   if (!authenticated) {
-    return <div className="mt-5 text-white text-center">Carregando...</div>;
+    return <Loader />;
   }
 
   return (
