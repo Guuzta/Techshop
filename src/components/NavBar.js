e42035bfae7460b75723ddeb0325c13bb1aef1cb
Guuzta <Guuhsilva13@outlook.com>
2026-01-30 21:00:54 -0300

    adicionando componente Link na NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -96,28 +96,28 @@ export default function NavBar() {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
               >
                 <MenuItem>
-                  <a
+                  <Link
                     href="/profile/myproducts"
                     className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
                   >
                     Meus produtos
-                  </a>
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
+                  <Link
+                    href="/profile/settings"
                     className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
                   >
                     Configurações
-                  </a>
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <a
+                  <Link
                     href="#"
                     className="block px-4 py-2 text-sm text-red-500 data-focus:bg-white/5 data-focus:outline-hidden"
                   >
                     Sair
-                  </a>
+                  </Link>
                 </MenuItem>
               </MenuItems>
             </Menu>
