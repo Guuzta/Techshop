8f682cdc671fbebdad1121d875cf8185ff4a6307
Guuzta <Guuhsilva13@outlook.com>
2026-01-31 12:12:53 -0300

    mostrando total de produtos na pagina settings

--- src/app/profile/settings/page.js
+++ src/app/profile/settings/page.js
@@ -12,11 +12,22 @@ import {
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Loader from "@/components/Loader";
+import ErrorToast from "@/components/ErrorToast";
 
 export default function Settings() {
   const [payload, setPayload] = useState("");
   const [userSince, setUserSince] = useState("");
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [showToast, setShowToast] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+
+  const handleShowToast = () => {
+    setShowToast(true);
+
+    setTimeout(() => {
+      setShowToast(false);
+    }, 3000);
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
@@ -28,12 +39,32 @@ export default function Settings() {
       const date = new Date(payload.createdAt);
       const userSince = date.toLocaleDateString("pt-br");
       setUserSince(userSince);
-
-      setIsLoading(false);
     }, 0);
-  }, []);
 
-  console.log(payload);
+    const fecthData = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/products/my`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        const data = await res.json();
+        const products = data.products.length;
+        setTotalProducts(products);
+
+        setIsLoading(false);
+      } catch (error) {
+        handleShowToast();
+        console.log(error);
+      }
+    };
+
+    fecthData();
+  }, []);
 
   return (
     <>
@@ -55,7 +86,7 @@ export default function Settings() {
           <div className="flex ">
             <div className="w-1/2 py-4 border border-gray-300/10 bg-gray-800/50 scheme-dark">
               <h1 className="text-4xl text-center font-medium text-gray-400 sm:text-6xl lg:text-6xl ">
-                {"0"}
+                {totalProducts}
               </h1>
               <h1 className="text-xl lg:text-2xl text-center italic text-green-400">{`Produtos cadastrados`}</h1>
             </div>
@@ -91,6 +122,8 @@ export default function Settings() {
           </div>
         </section>
       )}
+
+      <ErrorToast showToast={showToast} />
 
       <Footer />
     </>
